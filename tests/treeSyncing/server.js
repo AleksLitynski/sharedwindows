e11de3677e9d4ca8c729b4ffb7951fea9525ea6d
Aleksander Litynski <ajl5242@rit.edu>
2013-08-26 15:06:12 -0400

    non-persistant browser sync tree test
    is working?!?

--- tests/treeSyncing/server.js
+++ tests/treeSyncing/server.js
@@ -4,9 +4,12 @@ var fs      = require('fs');
 
 io.sockets.on('connection', function (socket) {
    
-    //socket.emit("change", {page: fs.readFileSync("thetree.txt", "utf8")});
+   // socket.emit("change", {page: fs.readFileSync("thetree.txt", "utf8")});
    
     socket.on('change', function(data) {
+       
+        //console.log(JSON.stringify(data.page));
+        
         fs.writeFile("thetree.txt", data.page, function(err) {}); 
         
         socket.broadcast.emit('change', data);
